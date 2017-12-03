0c1921452f3543803db7d134f9c1d10ef695779c
Graham Holden <graham.holden@elucidat.com>
2017-12-03 20:13:11 +0000

    add in an example of the decorator pattern - will need to alter but it's a start

--- decorator/DecoratorMain.hx
+++ decorator/DecoratorMain.hx
@@ -1,7 +1,48 @@
 class DecoratorMain {
 
     public static function main():Void {
-
-        
+        var email = new Email();
+        email.loadBody();
+         
+        /*
+         *  Email with Xmas Greetings
+         */
+         
+        var email = new Email();
+        var christmasEmailBodyemail = new ChristmasEmailBody(email);
+        christmasEmailBodyemail.loadBody();
     }
+}
+
+
+interface EmailBody {
+    public function loadBody():Void;
+}
+ 
+class Email implements EmailBody {
+    public function new (){}
+    public function loadBody() {
+        trace("This is Main Email body.<br />");
+    } 
+}
+ 
+class EmailBodyDecorator implements EmailBody {
+     
+    private var emailBody:EmailBody;
+     
+    public function new(emailBody:EmailBody) {
+        this.emailBody = emailBody;
+    }
+     
+    public function loadBody(){};
+     
+} 
+ 
+class ChristmasEmailBody extends EmailBodyDecorator {
+     
+    override public function loadBody() {
+         
+        trace("This is Extra Content for Christmas<br />"); 
+    }
+     
 }
